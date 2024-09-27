b3c4b5f5383a4d86f7be96a840fe983218991298
Pig Fang <g-plane@hotmail.com>
2024-09-27 16:05:34 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode'
+import { setupLspClient } from './lsp.js'
+
+export async function activate(context: vscode.ExtensionContext) {
+  const outputChannel = vscode.window.createOutputChannel('WebAssembly Language Tools')
+  context.subscriptions.push(outputChannel)
+
+  const client = await setupLspClient(outputChannel)
+  client.start()
+  context.subscriptions.push(client)
+}
+
+export function deactivate() {}
---
+++ src/lsp.ts
@@ -0,0 +1,37 @@
+import * as os from 'node:os'
+import * as vscode from 'vscode'
+import {
+  LanguageClient,
+  type LanguageClientOptions,
+  type ServerOptions,
+} from 'vscode-languageclient/node'
+import which from 'which'
+
+export async function setupLspClient(outputChannel: vscode.OutputChannel) {
+  const executablePath = await findExecutable()
+  outputChannel.appendLine(`Using server: ${executablePath}`)
+  const serverOptions: ServerOptions = {
+    run: {
+      command: executablePath,
+      args: [],
+    },
+    debug: {
+      command: executablePath,
+      args: ['--debug'],
+    },
+  }
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ language: 'wat' }],
+    outputChannel,
+  }
+  return new LanguageClient('wat', serverOptions, clientOptions)
+}
+
+async function findExecutable() {
+  const configuration = vscode.workspace.getConfiguration('wat')
+  const path = os.platform() === 'win32'
+    ? (configuration.get<string>('executablePath.win') || 'wat_server.exe')
+    : (configuration.get<string>('executablePath.unix') || 'wat_server')
+  return which(path)
+}
