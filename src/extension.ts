a9147f71b785965579b15aa6dca621dea75f19a5
Pig Fang <g-plane@hotmail.com>
2025-01-17 11:14:01 +0800

    feat: add command for downloading server manually

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,39 @@
 import * as vscode from 'vscode'
-import { setupLspClient } from './lsp.js'
+import { download } from './download.js'
+import { getDownloadedExePath, setupLspClient } from './lsp.js'
 
 export async function activate(context: vscode.ExtensionContext) {
   const outputChannel = vscode.window.createOutputChannel('WebAssembly Language Tools')
   context.subscriptions.push(outputChannel)
 
-  const client = await setupLspClient(context, outputChannel)
+  let client = await setupLspClient(context, outputChannel)
   if (client) {
     client.start()
     context.subscriptions.push(client)
-    context.subscriptions.push(
-      vscode.commands.registerCommand('wasmLanguageTools.restart', () => client.restart())
-    )
   }
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('wasmLanguageTools.restart', async () => {
+      if (client?.isRunning()) {
+        await client.restart()
+      }
+    })
+  )
+  context.subscriptions.push(vscode.commands.registerCommand(
+    'wasmLanguageTools.downloadServer',
+    async () => {
+      if (client) {
+        await client.stop()
+        await client.dispose()
+      }
+      await download(context, getDownloadedExePath(context))
+      client = await setupLspClient(context, outputChannel)
+      if (client) {
+        client.start()
+        context.subscriptions.push(client)
+      }
+    }
+  ))
 }
 
 export function deactivate() {}
--- src/lsp.ts
+++ src/lsp.ts
@@ -1,10 +1,6 @@
-import got from 'got'
-import jszip from 'jszip'
-import { createWriteStream } from 'node:fs'
 import * as fs from 'node:fs/promises'
 import * as os from 'node:os'
 import * as path from 'node:path'
-import { pipeline } from 'node:stream/promises'
 import * as vscode from 'vscode'
 import {
   LanguageClient,
@@ -12,6 +8,7 @@ import {
   type ServerOptions,
 } from 'vscode-languageclient/node'
 import which from 'which'
+import { download } from './download.js'
 
 export async function setupLspClient(
   context: vscode.ExtensionContext,
@@ -52,13 +49,7 @@ async function findExecutable(context: vscode.ExtensionContext) {
     // fall through
   }
 
-  const isWindows = os.platform() === 'win32'
-  // For example, on Linux, it will be
-  // ~/.config/Code/User/globalStorage/gplane.wasm-language-tools/wat_server
-  const downloadedExePath = path.join(
-    context.globalStorageUri.fsPath,
-    isWindows ? 'wat_server.exe' : 'wat_server'
-  )
+  const downloadedExePath = getDownloadedExePath(context)
   try {
     await fs.stat(downloadedExePath)
     return downloadedExePath
@@ -71,56 +62,15 @@ async function findExecutable(context: vscode.ExtensionContext) {
     'Yes',
     'No'
   )
-  if (answer !== 'Yes') {
-    return
+  if (answer === 'Yes') {
+    await download(context, downloadedExePath)
+    return downloadedExePath
   }
-  const zipPath = path.join(os.tmpdir(), 'wat_server.zip')
-  await vscode.window.withProgress({
-    location: vscode.ProgressLocation.Notification,
-    title: 'Downloading WebAssembly Language Tools',
-    cancellable: true,
-  }, async (progress, token) => {
-    const abortController = new global.AbortController()
-    token.onCancellationRequested(() => abortController.abort())
-
-    const stream = got.stream(getReleaseURL(), { signal: abortController.signal })
-    await new Promise((resolve, reject) => {
-      stream.on('response', resolve)
-      stream.once('error', reject)
-    })
-    let lastPercent = 0
-    stream.on('downloadProgress', ({ total, percent }) => {
-      if (total > 0) {
-        progress.report({ increment: (percent - lastPercent) * 100 })
-        lastPercent = percent
-      }
-    })
-    progress.report({ increment: 0 })
-    await pipeline(stream, createWriteStream(zipPath), { signal: abortController.signal })
-  })
-
-  const zip = await jszip.loadAsync(await fs.readFile(zipPath))
-  await fs.mkdir(context.globalStorageUri.fsPath, { recursive: true })
-  await pipeline(
-    zip.file(isWindows ? 'wat_server.exe' : 'wat_server')!.nodeStream(),
-    createWriteStream(downloadedExePath)
-  )
-  if (!isWindows) {
-    await fs.chmod(downloadedExePath, 0o755)
-  }
-  await fs.unlink(zipPath)
-  return downloadedExePath
 }
 
-function getReleaseURL() {
-  switch (os.platform()) {
-    case 'linux':
-      return 'https://github.com/g-plane/wasm-language-tools/releases/latest/download/wat_server-x86_64-linux.zip'
-    case 'darwin':
-      return os.arch() === 'x64'
-        ? 'https://github.com/g-plane/wasm-language-tools/releases/latest/download/wat_server-x86_64-macos.zip'
-        : 'https://github.com/g-plane/wasm-language-tools/releases/latest/download/wat_server-arm-macos.zip'
-    case 'win32':
-      return 'https://github.com/g-plane/wasm-language-tools/releases/latest/download/wat_server-x86_64-windows.zip'
-  }
+export function getDownloadedExePath(context: vscode.ExtensionContext) {
+  const isWindows = os.platform() === 'win32'
+  // For example, on Linux, it will be
+  // ~/.config/Code/User/globalStorage/gplane.wasm-language-tools/wat_server
+  return path.join(context.globalStorageUri.fsPath, isWindows ? 'wat_server.exe' : 'wat_server')
 }
