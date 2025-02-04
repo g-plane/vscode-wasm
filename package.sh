17aa9b66fd098dafdf8a5258efaf1a998951afd4
Pig Fang <g-plane@hotmail.com>
2025-02-04 17:47:41 +0800

    revert "rename "arm" to "arm64""
    This reverts commit c317f0ceeae9a2342da53db84091c87e9225660b.

--- package.sh
+++ package.sh
@@ -15,7 +15,7 @@ rm wat_server-x86_64-macos.zip
 pnpx @vscode/vsce package --target darwin-x64
 rm -r bin
 
-wget https://github.com/g-plane/wasm-language-tools/releases/latest/download/wat_server-arm64-macos.zip
+wget https://github.com/g-plane/wasm-language-tools/releases/latest/download/wat_server-arm-macos.zip
 unzip -o wat_server-arm-macos.zip -d bin
 rm wat_server-arm-macos.zip
 pnpx @vscode/vsce package --target darwin-arm64
